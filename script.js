57eabf238a51c93c136fed7f591503767611d20f
Ugyen Lama <fugla001@edu.xamk.fi>
2024-01-13 13:45:25 +0200

    first assignment submission

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.getElementById("actionButton").addEventListener("click", function() {
-    alert("Button clicked!");
+
+document.getElementById('currentYear').innerText = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function() {
+    var descriptionParagraph = document.getElementById('description');
+    var actionButton = document.getElementById('actionButton');
+
+    actionButton.addEventListener('click', function() {
+        toggleVisibility(descriptionParagraph);
+        toggleButtonText(actionButton);
+    });
+
+    document.getElementById('currentYear').innerText = new Date().getFullYear();
 });
+
+function toggleVisibility(element) {
+    element.style.display = (element.style.display === 'none' || element.style.display === '') ? 'block' : 'none';
+}
+
+function toggleButtonText(button) {
+    button.innerText = (button.innerText === "Hide Text") ? "Show Text" : "Hide Text";
+}
+
+document.getElementById('description').style.display = 'block';
